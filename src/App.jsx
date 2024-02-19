472702d8317cf7cc4d290602f3138c0815d275f9
mernstackjs <mernstack.js@gmail.com>
2024-02-19 11:41:00 +0100

    tijaabo

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,5 @@
 import React from "react";
-import Register from "./Register";
-import Login from "./Login";
-import axios from "axios";
-import ProtectedRoute from "./Protecedroute";
+
 export default function App() {
-  const fetchUser = async () => {
-    const res = await axios.get("http://localhost:3003/users");
-  };
-  fetchUser();
-  return (
-    <div>
-      <h1>users is here</h1>
-      <Register />
-      <Login />
-      <ProtectedRoute />
-    </div>
-  );
+  return <div>App</div>;
 }
